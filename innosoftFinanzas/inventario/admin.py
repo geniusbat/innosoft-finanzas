5adb47ef8c862a0eb7480d1b0e81e5182453ff19
Geniusbat <moligus@gmail.com>
2022-01-03 13:22:36 +0100

    Rehaciendo lo que Javi deshizo

--- innosoftFinanzas/inventario/admin.py
+++ innosoftFinanzas/inventario/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
 from inventario.models import Producto
 from necesidades.models import Necesidad
-
-# Register your models here.
-
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "categoria", "unidades", "valorMonetario")
-    search_fields= ("nombre", "categoria")
-    list_filter = ("categoria",)
-
-admin.site.register(Producto, ProductoAdmin)
-admin.site.register(Necesidad)
