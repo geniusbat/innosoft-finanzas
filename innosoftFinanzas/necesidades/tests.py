e3e877eb2769143583d0dcdc92f1a7a9422c5151
victor <victormp00@gmail.com>
2022-01-04 05:31:06 +0100

    Feature/ pruebas visuales de necesidad

--- innosoftFinanzas/necesidades/tests.py
+++ innosoftFinanzas/necesidades/tests.py
@@ -1,6 +1,19 @@
 from django.test import TestCase
 from necesidades.models import Comite, Necesidad
 import json
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from base.tests import BaseTestCase
+from innosoftFinanzas import settings
+from inventario.models import Producto, Categoria
+import json
+# Create your tests here.
+from django.test import TestCase
 class NecesidadesTest(TestCase):
     def setUp(self):
         super().setUp()
@@ -66,3 +79,43 @@ class NecesidadesTest(TestCase):
         y = json.loads(s)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(y["nombre"], 'Prueba 1')
+class NecesidadVisualTest(StaticLiveServerTestCase):
+    def setUp(self):
+        super().setUp()
+
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        options.add_argument('--disable-gpu')
+        options.add_argument("--start-maximized")
+
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        self.driver = webdriver.Chrome(options=options)
+
+        self.vars = {}
+
+    def tearDown(self):
+        super().tearDown()
+        self.driver.quit()
+        self.base.tearDown()
+
+    def test_createComite(self):
+        #no hay delete comite por ahora
+        #self.driver.get('http://127.0.0.1:8000/inventario/productos')
+        #self.driver.get(f'{self.live_server_url}/inventario/productos')
+        self.driver.get(settings.BASE_LOCAL_URL + '/necesidades/necesidades')
+
+        self.driver.find_element(By.ID, 'botonAniadirComite').click()
+
+        time.sleep(1)
+
+        self.driver.find_element(By.ID, 'comite').click()
+        self.driver.find_element(By.ID, 'comite').send_keys("comitePrueba")
+        self.driver.find_element(By.ID, '_save').click()
+
+        tablaCategoria = self.driver.find_element(By.ID, 'tablaComites')
+        rowsCategorias = tablaCategoria.find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')
+
+        self.assertTrue(len(rowsCategorias) > 0)
+
