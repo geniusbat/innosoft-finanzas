871c1e7b4eb143aec7f6dd24bcf92c54057abbdf
diego <diegillos@alum.us.es>
2022-01-05 18:37:33 +0100

    Implementar eliminación de necesidades y comités

--- innosoftFinanzas/necesidades/models.py
+++ innosoftFinanzas/necesidades/models.py
@@ -8,7 +8,6 @@ class Comite(models.Model):
 
 class Necesidad(models.Model):
     nombre= models.CharField(max_length=80)
-    #producto= models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidadNecesitada= models.IntegerField()
     comite= models.ForeignKey(Comite, on_delete=models.CASCADE)
     descripcion = models.TextField()
